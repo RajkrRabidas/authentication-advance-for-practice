e14f004e315ed7c0fc0edc3d0401ecad41c02503
RajkrRabidas <singhkabir05185@gmail.com>
2026-05-28 17:43:21 +0530

    feat: add Redis client configuration and update password validation in registration schema

---
+++ Backend/config/redis.js
@@ -0,0 +1,19 @@
+const { createClient } = require("redis");
+
+const redisUrl = process.env.REDIS_URL;
+
+if (!redisUrl) {
+  console.log("redis url missing");
+  process.exit(1);
+}
+
+const redisClient = createClient({
+  url: redisUrl,
+});
+
+redisClient
+  .connect()
+  .then(() => console.log("connect to redis"))
+  .catch(console.error);
+
+module.exports = redisClient;
--- Backend/config/zod.js
+++ Backend/config/zod.js
@@ -3,7 +3,7 @@ const {z} = require("zod")
 const registerSchema = z.object({
     "name": z.string().min(3, "Name must be at least 3 characters long"),
     "email": z.string().email("Invalid email address"),
-    "password": z.string().min(6, "Password must be at least 6 characters long")
+    "password": z.string().min(8, "Password must be at least 8 characters long")
 })
 
 module.exports = registerSchema
