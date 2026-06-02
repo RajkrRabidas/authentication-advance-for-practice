0ac84bbb7bc21979fb9c0a90922bc93e00f07797
RajkrRabidas <singhkabir05185@gmail.com>
2026-06-02 20:36:13 +0530

    fix: enhance user registration with rate limiting and error handling

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -3,6 +3,7 @@ const registerSchema = require("../config/zod");
 const TryCatch = require("../middlewares/tryCatch");
 const { sanitize } = require("mongo-sanitize");
 const userModel = require("../models/User.model");
+const { getVerifyEmailHtml } = require("../config/html");
 
 const registerUser = TryCatch(async (req, res) => {
   const sanitizeBody = sanitize(req.body);
@@ -33,8 +34,36 @@ const registerUser = TryCatch(async (req, res) => {
   try {
       const {name, email, password} = validation.data
 
+      const rateLimte = `register-rate-limit:${req,ip},:${email}`
+
+      if(await redisClient.get(rateLimte)){
+        return res.status(429).json({message: "Too many request, please try agian later"})
+      }
+
+      const exitingUser = await userModel.findOne({email})
+
+      if(exitingUser){
+        return res.status(429).json({message: "User already exit"})
+      }
+
+      const hashPassword = await becryt.hash(password, 10)
+      const verifyToken = await crypto.randomBytes(32).toString("hex")
+      const verifyKey = `verifyKey:${verifyToken}`
+      const dataToStore = JSON.stringify({email, name, password: hashPassword})
+
+      await redisClient.set(verifyKey, dataToStore, {EX:300})
+
+      const subject = "verify your account"
+      const html = getVerifyEmailHtml({email, token: verifyToken})
+      await sendEmail({to: email, subject, html})
+
+      await redisClient.set(rateLimte, "true", {EX: 60})
+
+      return res.status(200).json({message: "Verification email sent, please check your inbox"})
 
   } catch (error) {
-    
-  }WS
+      console.error("Error in registerUser controller:", error);
+      return res.status(500).json({message: "Internal Server Error"}) 
+  }
 });
+
