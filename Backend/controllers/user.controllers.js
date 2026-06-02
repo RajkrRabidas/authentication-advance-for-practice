7dbd301eaef6827ce098ac5d0bafaafea2330eff
RajkrRabidas <singhkabir05185@gmail.com>
2026-06-02 18:50:54 +0530

    fix: correct variable naming and remove commented-out registration logic

--- Backend/controllers/user.controllers.js
+++ Backend/controllers/user.controllers.js
@@ -2,7 +2,7 @@ const redisClient = require("../config/redis");
 const registerSchema = require("../config/zod");
 const TryCatch = require("../middlewares/tryCatch");
 const { sanitize } = require("mongo-sanitize");
-const UserModel = require("../models/User.model");
+const userModel = require("../models/User.model");
 
 const registerUser = TryCatch(async (req, res) => {
   const sanitizeBody = sanitize(req.body);
@@ -31,42 +31,10 @@ const registerUser = TryCatch(async (req, res) => {
   }
 
   try {
-    const { name, email, password } = validation.data;
+      const {name, email, password} = validation.data
 
-    const ratelimitKey = await `register-rate-limit${req.ip}:${email}`;
-    if (await redisClient.get(ratelimitKey)) {
-      return res
-        .status(429)
-        .json({ message: "Too many request, try agian later" });
-    }
 
-    const existingUser = await UserModel.findOne({ email });
-
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const verifyToken = crypto.randomBytes(32).toString("hex");
-    const verifyKey = `verify-token:${verifyToken}`;
-
-    const dataToStore = Json.stringify({
-      name,
-      email,
-      password: hashedPassword,
-    });
-
-    await redisClient.set(verifyKey, dataToStore, { EX: 300 });
-
-    const subject = "Verify your email";
-    const html = getVerifyEmailHtml({ email, token: verifyToken });
-    await sendMail({ email, subject, html });
-
-    await redisClient.set(ratelimitKey, "true", { EX: 60 }); // 1 min rate limit
-
-    res.status(200).json({
-      message:
-        "Registration successful. Please check your email to verify your account.",
-    });
-  } catch (error) {}
+  } catch (error) {
+    
+  }WS
 });
